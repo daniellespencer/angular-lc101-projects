35e6104bbfa2da5eb41ee5dc7a4d00e0dc7f5e8c
Danielle Spencer <d.m.spencer.0486@gmail.com>
2019-11-19 15:42:05 -0600

    push studio updates

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCaWAsaQvxcNyx22XoiP7OirQZv4-NMDc_76H5mIt489_6nYY-Vg&s';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRT2XK4W0i7icS84Yq5VHpwZ9anmDXoOFQZX0anUhWxcpL5du_&s';
 
   constructor() { }
 
